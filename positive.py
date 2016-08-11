a9fdff29ff09ef9ff37fc2f694f2504be4222a69
priyashan1621 <dharshinisisters@gmail.com>
2016-08-11 16:02:14 +0530

    Update positive.py

--- positive.py
+++ positive.py
@@ -1,8 +1,8 @@
-n=int(input('Enter the number '))
-if(n!=0):
-    if(n>0):
-        print('Positive')
-    else:
-        print('Negative')
-else:
-    print('Zero')
+number=int(input('Enter the number '))
+if number==0:
+    print("Zero")
+else if number>0:
+    print("Positive")
+elif number<0:
+    print("Negative")
+    
